534b8cc921a7f6aa5781b1728c7836e296b2cc4d
Navian <primeguyy@gmail.com>
2021-02-07 21:59:18 +0000

    Navigation header added

--- js/script.js
+++ js/script.js
@@ -1,8 +1,31 @@
+// Hamburger navigation
+const navigation = document.querySelector('.navigation');
+const navBar = document.querySelector('.nav-bar');
+const navList = document.querySelectorAll('.nav-list');
+const hamburgerOpen= document.querySelector('.ham-open');
+const hamburgerClose= document.querySelector('.ham-close');
+
+hamburgerOpen.addEventListener('click', () => {
+    navigation.classList.add('mobile-nav');
+    hamburgerOpen.style.display = 'none';
+    hamburgerClose.style.display = 'block';
+
+});
+
+hamburgerClose.addEventListener('click', () => {
+    navigation.classList.remove('mobile-nav');
+    hamburgerClose.style.display = 'none';
+    hamburgerOpen.style.display = 'block';
+
+});
+
+
+// adding event listener to each side to listen for the mouse hover event
+
 const slideWrapper = document.querySelector('.slide-wrapper');
 const slideLeft = document.querySelector('.left');
 const slideRight = document.querySelector('.right');
 
-// add event listener to each side to listen for the mouse hover event
 
 slideLeft.addEventListener('mouseenter', () => {
     slideWrapper.classList.add('hovered-left');
